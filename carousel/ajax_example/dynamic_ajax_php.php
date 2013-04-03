256669dff6bac28af480cc2a2a4db69dd2173bf4
Kevin Whalen <KWhalen2@kent.edu>
2013-04-03 00:22:50 -0400

    copied jCarousel ajax example. uses a substitued php file

---
+++ carousel/ajax_example/dynamic_ajax_php.php
@@ -0,0 +1,5 @@
+<?php
+echo "<data>  <total>10</total>  <image>http://static.flickr.com/66/199481236_dc98b5abb3_s.jpg</image>  <image>http://static.flickr.com/75/199481072_b4a0d09597_s.jpg</image></data>";
+
+?>
+
