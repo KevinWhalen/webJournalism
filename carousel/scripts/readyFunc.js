d77a62f6edc62edd33beb20a78d2d85e71f07f37
Kevin Whalen <KWhalen2@kent.edu>
2013-04-06 22:39:21 -0400

    implemented carousel ready to be joined with scripts that pull from the video database

---
+++ carousel/scripts/readyFunc.js
@@ -0,0 +1,21 @@
+/* readyFunc.js
+ * 
+ * Kevin Whalen
+ * 3/19/13
+*/
+
+$(document).ready(function(){
+
+  $('#theCarousel').jcarousel({
+    // Configuration goes here
+  });
+
+/*
+  $(window).resize(function(){
+    var width = window.innerWidth * 0.77;
+    var height = width * 0.50;
+    //resizeFrame(width, height);
+  });
+*/
+
+});
