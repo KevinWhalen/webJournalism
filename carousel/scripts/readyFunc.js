68b30ff812829c5cc3a5659d4ca1bd7d716cdfd9
Kevin Whalen <KWhalen2@kent.edu>
2013-04-09 11:46:48 -0400

    pre-migration formatting update

--- carousel/scripts/readyFunc.js
+++ carousel/scripts/readyFunc.js
@@ -76,7 +76,6 @@ $('#showdata').append("<p>"+cat+"</p>"); //debugging
 				$.getJSON(
 				'scripts/jsonData/thumbnails.php?category='+d.categories, 
 				function(categoryData){
-console.log(categoryData);
 					populateAllThumbnails(categoryData);
 				});
 			});
